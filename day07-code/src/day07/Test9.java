af8b6142aed8ae1caf15839aaf3901ffaeb4b081
wan <1940802837l@gmail.com>
2024-06-25 00:54:57 +0800

    day07 Test9

---
+++ day07-code/src/day07/Test9.java
@@ -0,0 +1,13 @@
+package day07;
+
+public class Test9 {
+    public static void main(String[] args) {
+        //二维数组静态初始化
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6, 7}
+        };
+        //动态初始化
+        int[][] arr2 = new int[2][3];
+    }
+}
